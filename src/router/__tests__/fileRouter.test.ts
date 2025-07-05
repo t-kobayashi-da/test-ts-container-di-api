b60a941dfaf6d3a508690c8aedb2435fd6d0ea2b
t-kobayashi-da <t_kobayashi@dreamarts.co.jp>
2025-07-05 15:27:36 +0900

    fix: Controllerもapp.tsで生成・注入する

--- src/router/__tests__/fileRouter.test.ts
+++ src/router/__tests__/fileRouter.test.ts
@@ -1,16 +1,17 @@
 import request from 'supertest';
 import express from 'express';
-
-//import express from 'express';
-
 import { fileRouter } from '../fileRouter';
+import { Request, Response } from 'express';
 
 describe('FileRouter', () => {
   it('GET / returns text', async () => {
-    const mockService = { getTextFile: jest.fn().mockReturnValue('router test') };
-
+    // モックのハンドラー関数を用意
+    const mockHandler = (_req: Request, res: Response) => {
+        res.type('text/plain').send('router test');
+    };
+ 
     const app = express();
-    app.use('/file', fileRouter(mockService as any));
+    app.use('/file', fileRouter(mockHandler));
 
     const res = await request(app).get('/file');
 
--- src/router/fileRouter.ts
+++ src/router/fileRouter.ts
@@ -1,9 +1,11 @@
-import { Router } from 'express';
-import { FileService } from '../service/fileService';
-import { getFileHandler } from '../controller/fileController';
+import { Router, Request, Response } from 'express';
+//import { Router } from 'express';
+//import { FileService } from '../service/fileService';
+//import { getFileHandler } from '../controller/fileController';
 
-export const fileRouter = (fileService: FileService) => {
+//export const fileRouter = (fileService: FileService) => {
+export const fileRouter = (getFileHandler: (req: Request, res: Response) => void) => {
   const router = Router();
-  router.get('/', getFileHandler(fileService));
+  router.get('/', getFileHandler);
   return router;
 };
