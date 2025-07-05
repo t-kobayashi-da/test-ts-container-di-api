a099df9717ad97464f5a4120729a0928b1fa37a8
t-kobayashi-da <t_kobayashi@dreamarts.co.jp>
2025-07-05 15:32:11 +0900

    fix: app.ts の更新し忘れ

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@ import express from 'express';
 import { fileRouter } from './router/fileRouter';
 import { FileService } from './service/fileService';
 import { FileRepository } from './repository/fileRepository';
+import { getFileHandler } from './controller/fileController';
 
 export const createApp = () => {
   const app = express();
 
   const fileRepository = new FileRepository();
   const fileService = new FileService(fileRepository);
+  const fileHandler = getFileHandler(fileService);
 
-  app.use('/file', fileRouter(fileService));
+  app.use('/file', fileRouter(fileHandler));
 }
